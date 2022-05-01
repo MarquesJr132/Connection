7585640880fe723c47ab52c452353bc60d3cf9b9
Marques Junior <81861759+MarquesJr132@users.noreply.github.com>
2022-05-01 19:25:38 +0200

    ASDSA

--- src/main/java/com/example/connection/config/ApiConfig.java
+++ src/main/java/com/example/connection/config/ApiConfig.java
@@ -2,11 +2,17 @@ package com.example.connection.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class ApiConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
@@ -15,5 +21,18 @@ public class ApiConfig {
                 registry.addMapping("/cliente/**").allowedOrigins("http://localhost:63342/");
             }
         };
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration corsConfiguration=new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:63342"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Acess-Control-Allow-Origin","Content-Type","Accept",
+                "Authorization","Origin","Accept","X-Requested-With","Acess-Control-Request-Method","Acess-Control-Request-Headers"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        UrlBasedCorsConfigurationSource url=new UrlBasedCorsConfigurationSource();
+        url.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsFilter(url);
     }
 }
